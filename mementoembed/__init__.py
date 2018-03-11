4b3a4a60d132c9336a88374d03705127c150b60c
Shawn M. Jones <jones.shawn.m@gmail.com>
2018-03-11 15:29:38 -0600

    initial research on using flask, and the start of some mockups

---
+++ mementoembed/__init__.py
@@ -0,0 +1,1 @@
+from .mementoembed import app
---
+++ mementoembed/mementoembed.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'HEAD'])
+def front_page():
+    return render_template('front_page.html', urim=urim)
+
+@app.route('/socialcard/<path:uri>', methods=['GET', 'HEAD'])
+def make_social_card(uri=None):
+    return render_template('social_card.html', urim=urim)
+
+
+if __name__ == '__main__':
+    app.run()
