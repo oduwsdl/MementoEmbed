0ffab9d5fda951bdaf49513113948f30ca70cad7
Shawn M. Jones <jones.shawn.m@gmail.com>
2018-06-14 16:41:34 -0600

    more work for #32

--- tests/deploycache.sh
+++ tests/deploycache.sh
@@ -3,6 +3,10 @@
 # Thanks: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ ! -d /tmp/mementoembed ]; then
+    mkdir -p /tmp/mementoembed
+fi
+
 cp testingcache.tar.bz2 /tmp/mementoembed/
 
 cd /tmp/mementoembed/
