659f230e0c78fd035c91438e22f570c003cb930d
Shawn M. Jones <jones.shawn.m@gmail.com>
2018-07-16 15:45:30 -0600

    fixes for waitress deployment issue that turned out to be a setup.py installation issue

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 redis-server --daemonize yes --save ""
-# waitress-serve --port=5550 --call mementoembed:create_app
-export FLASK_APP=mementoembed
-flask run --host 0.0.0.0 --port 5550
+waitress-serve --port=5550 --call mementoembed:create_app
