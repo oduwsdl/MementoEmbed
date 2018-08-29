3029035f22771210a4861ce501a26ac0ca12b212
Shawn M. Jones <jones.shawn.m@gmail.com>
2018-08-29 14:27:51 -0600

    fixes #124, there are mixed-content errors when users connect to MementoEmbed with HTTPS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Shawn M. Jones'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2018.08.07.030502'
+release = '0.2018.08.29.202630'
 
 
 # -- General configuration ---------------------------------------------------
