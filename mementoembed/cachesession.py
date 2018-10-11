c08d0d7455e67f23bd39fd3ab28a9bdd8739e56e
Shawn M. Jones <jones.shawn.m@gmail.com>
2018-10-10 20:49:30 -0600

    A workaround for Brotli encoding that is not suppored by the requests library

--- mementoembed/cachesession.py
+++ mementoembed/cachesession.py
@@ -1,9 +1,56 @@
 import logging
 
 import requests
+import brotli
 import requests_cache
 
-module_logger = logging.getLogger('mementoembed.cachesession')
+module_logger = logging.getLogger('mementoembed.cachesession')   
+
+class BrotliResponse:
+    """
+        This class exists because requests had not yet implemented the
+        Brotli content-encoding ('br').
+    """
+
+    def __init__(self, response):
+        self.response = response
+        self._content = False
+        self.apparent_encoding = self.response.apparent_encoding
+        self.links = self.response.links
+        self.headers = self.response.headers
+        self.status_code = self.response.status_code
+        self.url = self.response.url
+
+    @property
+    def content(self):
+
+        if self._content is False:
+
+            self._content = self.response.content
+
+            if 'content-encoding' in self.response.headers:
+                if self.response.headers['content-encoding'] == 'br':
+                    self._content = brotli.decompress(self._content)
+
+        return self._content
+
+    @property
+    def text(self):
+
+        content = None
+        encoding = self.response.encoding
+
+        if self.response.encoding is None:
+            encoding = self.apparent_encoding
+
+        try:
+            content = str(self.content, encoding, errors='replace')
+        except (LookupError, TypeError):
+            content = str(self.content, errors='replace')
+
+        return content
+
+                
 
 class CacheSession:
 
@@ -20,7 +67,10 @@ class CacheSession:
 
         req_headers = {}
 
+        req_headers['accept-encoding'] = "gzip, deflate"
+
         for key in headers:
+            # Note that this will allow the caller to overwrite the accept-encoding
             req_headers[key] = headers[key]
 
         if use_referrer:
@@ -32,5 +82,9 @@ class CacheSession:
         module_logger.debug("request headers sent were {}".format(response.request.headers))
         module_logger.debug("response status: {}".format(response.status_code))
         module_logger.debug("response headers: {}".format(response.headers))
+
+        if 'content-encoding' in response.headers:
+            if response.headers['content-encoding'] == 'br':
+                response = BrotliResponse(response)
 
         return response
