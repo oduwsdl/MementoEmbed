9ca73619dc5264647727361c84c634b2d0827301
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-02-12 13:11:59 -0700

    updated dependencies to address security issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Shawn M. Jones'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2019.02.12.193745'
+release = '0.2019.02.12.201118'
 
 
 # -- General configuration ---------------------------------------------------
--- mementoembed/version.py
+++ mementoembed/version.py
@@ -1,3 +1,3 @@
 __appname__ = "MementoEmbed"
-__appversion__ = '0.2019.02.12.193801'
+__appversion__ = '0.2019.02.12.201118'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
