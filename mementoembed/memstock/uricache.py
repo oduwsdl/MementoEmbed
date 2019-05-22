ca46d2399b9961b7a84ead4eb88d73de1b875dde
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-05-22 15:21:25 -0600

    improvements after testing

--- mementoembed/memstock/uricache.py
+++ mementoembed/memstock/uricache.py
@@ -6,10 +6,13 @@ import datetime
 from sqlite3 import OperationalError, ProgrammingError
 
 import requests
-
+from requests.models import Response
 from requests.structures import CaseInsensitiveDict
 
 module_logger = logging.getLogger('mementoembed.memstock.uricache')
+
+# this whole implementation exists because requests_cache
+# would occasionally return incorrect content for a URI
 
 # TODO: create minimal response object
 
@@ -24,13 +27,32 @@ class URICacheError(Exception):
 class URINotInCacheError(URICacheError):
     pass
 
+def dbtable_exists_already(dbconn):
+
+    query = '''SELECT name from sqlite_master where type="table"'''
+
+    c = dbconn.cursor()
+    c.execute(query, ())
+
+    records = c.fetchall()
+
+    if len(records) == 1:
+        return records[0][0] == "URICACHE"
+    else:
+        return False
+
 def create_uricache_table(dbconn):
 
     query = '''CREATE TABLE URICACHE (
             uri TEXT,
             request_headers TEXT,
+            request_method TEXT,
             response_status INTEGER,
+            response_reason TEXT,
+            response_elapsed INTEGER,
             response_headers TEXT,
+            response_encoding TEXT,
+            response_history TEXT,
             response_content BLOB, 
             observation_datetime TIMESTAMP)'''
 
@@ -42,7 +64,7 @@ def uri_exists_already(uri, dbconn):
     exists = False
 
     try:
-        get_uri_timestamp(uri, dbconn)
+        get_uri_datafield(uri, dbconn, 'observation_datetime')
         exists = True
     except URINotInCacheError:
         pass
@@ -64,13 +86,10 @@ def purgeuri(uri, dbconn):
 
 def purgeuri_if_expired(uri, dbconn, expiration_delta):
 
-    if (datetime.datetime.now() - get_uri_timestamp(uri, dbconn)) > expiration_delta:
+    if (datetime.datetime.now() - get_uri_datafield(uri, dbconn, 'observation_datetime')) > expiration_delta:
         purgeuri(uri, dbconn)
 
-def saveuri(uri, dbconn, session=None, headers={}, expiration_delta=None):
-
-    if session is None:
-        session = requests.Session()
+def saveuri(uri, dbconn, session, expiration_delta=None):
 
     if expiration_delta is not None:
         purgeuri_if_expired(uri, dbconn, expiration_delta)
@@ -78,17 +97,42 @@ def saveuri(uri, dbconn, session=None, headers={}, expiration_delta=None):
     try:
         r = session.get(uri)
 
-        query = "INSERT INTO URICACHE " \
-            "(uri, request_headers, response_status, response_headers, " \
-            "response_content, observation_datetime) " \
-            "VALUES(?, ?, ?, ?, ?, ?)"
+        query = '''INSERT INTO URICACHE (
+                uri, 
+                request_headers,
+                request_method,
+                response_status,
+                response_reason,
+                response_elapsed,
+                response_headers,
+                response_encoding,
+                response_history,
+                response_content,
+                observation_datetime
+            ) VALUES(
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?)'''
         queryargs = (
-            uri, 
-            json.dumps(dict(r.request.headers)), 
-            r.status_code, 
+            uri,
+            json.dumps(dict(r.request.headers)),
+            r.request.method,
+            r.status_code,
+            r.reason,
+            r.elapsed.microseconds,
             json.dumps(dict(r.headers)),
+            r.encoding,
+            json.dumps(r.history),
             r.content, 
-            datetime.datetime.now()
+            datetime.datetime.utcnow()
             )
 
         c = dbconn.cursor()
@@ -98,11 +142,11 @@ def saveuri(uri, dbconn, session=None, headers={}, expiration_delta=None):
     except (OperationalError, ProgrammingError) as e:
         module_logger.exception("Database error for uri: {}".format(uri))
         raise e
-    
-def get_uri_response_body(uri, dbconn):
 
-    query = "SELECT response_content FROM URICACHE WHERE URI == ?"
-    queryargs = (uri, )
+def get_uri_datafield(uri, dbconn, field):
+
+    query = "SELECT {} from URICACHE WHERE URI == ?".format(field)
+    queryargs = (uri,)
     c = dbconn.cursor()
     c.execute(query, queryargs)
 
@@ -113,36 +157,36 @@ def get_uri_response_body(uri, dbconn):
     else:
         raise URINotInCacheError("This URI is not in the cache: {}".format(uri))
 
-def get_uri_response_headers(uri, dbconn):
+class URICache:
 
-    query = "SELECT response_headers FROM URICACHE WHERE URI == ?"
-    queryargs = (uri, )
-    c = dbconn.cursor()
-    c.execute(query, queryargs)
+    def __init__(self, dbname, session, expiration_delta):
 
-    records = c.fetchall()
+        self.dbconn = sqlite3.connect(dbname)
+        self.session = session
+        self.expiration_delta = expiration_delta
 
-    if len(records) == 1:
-        return CaseInsensitiveDict(json.loads(records[0][0]))
-    else:
-        raise URINotInCacheError("This URI is not in the cache: {}".format(uri))
+        if dbtable_exists_already(self.dbconn) == False:
+            create_uricache_table(self.dbconn)
 
-def get_uri_timestamp(uri, dbconn):
+    def get(self, uri, headers={}, timeout=None):
 
-    query = "SELECT observation_datetime FROM URICACHE WHERE URI == ?"
-    queryargs = (uri, )
-    c = dbconn.cursor()
-    c.execute(query, queryargs)
+        if not uri_exists_already(uri, self.dbconn):
+            saveuri(uri, self.dbconn, session=self.session)
 
-    records = c.fetchall()
+        req_headers = CaseInsensitiveDict(json.loads(get_uri_datafield(uri, self.dbconn, 'request_headers')))
+        req_method = get_uri_datafield(uri, self.dbconn, 'request_method')
+        request = requests.Request(req_method, uri, headers=req_headers)
+        request.prepare()
 
-    if len(records) == 1:
+        response = Response()
+        response.request = request
+        response.status_code = get_uri_datafield(uri, self.dbconn, 'response_status')
+        response.reason = get_uri_datafield(uri, self.dbconn, 'response_reason')
+        response.elapsed = datetime.timedelta(microseconds=get_uri_datafield(uri, self.dbconn, 'response_elapsed'))
+        response.encoding = get_uri_datafield(uri, self.dbconn, 'response_encoding')
+        response.history = json.loads(get_uri_datafield(uri, self.dbconn, 'response_history'))
+        response.headers = CaseInsensitiveDict(json.loads(get_uri_datafield(uri, self.dbconn, 'response_headers')))
+        response._content = get_uri_datafield(uri, self.dbconn, 'response_content')
+        response.url = uri
 
-        timestamp = records[0][0]
-        
-        if type(timestamp) == str:
-            return datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
-        else:
-            return timestamp
-    else:
-        raise URINotInCacheError("This URI is not in the cache: {}".format(uri))
+        return response
