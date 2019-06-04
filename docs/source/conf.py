218c0a4b86866f969c4f1d025f4ca6f8f86da03a
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-06-04 17:22:58 -0600

    raising version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Shawn M. Jones'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2019.06.03.223423'
+release = '0.2019.06.04.232247'
 
 
 # -- General configuration ---------------------------------------------------
--- mementoembed/version.py
+++ mementoembed/version.py
@@ -1,3 +1,3 @@
 __appname__ = "MementoEmbed"
-__appversion__ = '0.2019.06.03.223423'
+__appversion__ = '0.2019.06.04.232247'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
