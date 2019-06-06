2e439f16b2fad005ecd257808a9e54e3b6ac13a9
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-06-05 23:08:55 -0600

    raising version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Shawn M. Jones'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2019.06.06.035957'
+release = '0.2019.06.06.050844'
 
 
 # -- General configuration ---------------------------------------------------
