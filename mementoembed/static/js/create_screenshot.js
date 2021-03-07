685aebcd211b75797de30faa2e2c47df364d1df5
himarshaj <jayanetti.hr@gmail.com>
2021-03-07 00:59:12 -0500

    Introducing waitForNetworkIdle() to control the idle time until page loads

--- mementoembed/static/js/create_screenshot.js
+++ mementoembed/static/js/create_screenshot.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 
+
 (async () => {
 
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: true, 
     ignoreHTTPSErrors: true,
     args:['--no-sandbox']}
     );
@@ -23,10 +24,47 @@ const puppeteer = require('puppeteer');
       timeout: 5000000
     });
 
-  //Set wait time before screenshotURI
-  await page.waitFor(2000);
+  //Set wait time before screenshotURI - equivalent to 'networkidle0'
+  await Promise.all([
+    waitForNetworkIdle(page, 2000, 0), 
+  ]);
 
   await page.screenshot({path: process.env.THUMBNAIL_OUTPUTFILE});
 
   await browser.close();
 })();
+
+//Functions required to control the network idle time 
+
+function waitForNetworkIdle(page, timeout, maxInflightRequests = 0) {
+  page.on('request', onRequestStarted);
+  page.on('requestfinished', onRequestFinished);
+  page.on('requestfailed', onRequestFinished);
+
+  let inflight = 0;
+  let fulfill;
+  let promise = new Promise(x => fulfill = x);
+  let timeoutId = setTimeout(onTimeoutDone, timeout);
+  return promise;
+
+  function onTimeoutDone() {
+    page.removeListener('request', onRequestStarted);
+    page.removeListener('requestfinished', onRequestFinished);
+    page.removeListener('requestfailed', onRequestFinished);
+    fulfill();
+  }
+
+  function onRequestStarted() {
+    ++inflight;
+    if (inflight > maxInflightRequests)
+      clearTimeout(timeoutId);
+  }
+  
+  function onRequestFinished() {
+    if (inflight === 0)
+      return;
+    --inflight;
+    if (inflight === maxInflightRequests)
+      timeoutId = setTimeout(onTimeoutDone, timeout);
+  }
+}
