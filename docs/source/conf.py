1e8247c5fffb2636d6ef863f259e2a86dd46eab0
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-11-14 18:47:26 -0700

    RTD fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Shawn M. Jones'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.5.20211115014414'
+release = '0.5.20211115014726'
 
 # -- General configuration ---------------------------------------------------
 
