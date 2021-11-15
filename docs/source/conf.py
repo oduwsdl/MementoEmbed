92bea783505ab4c709e685c84483e42d0f2e7f03
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-11-14 18:00:06 -0700

    minor changes for installer #188

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Shawn M. Jones'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.20211114225555'
+release = '0.5.20211115010006'
 
 # -- General configuration ---------------------------------------------------
 
--- mementoembed/version.py
+++ mementoembed/version.py
@@ -1,3 +1,3 @@
 __appname__ = "MementoEmbed"
-__appversion__ = '0.20211114225555'
+__appversion__ = '0.5.20211115010006'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
